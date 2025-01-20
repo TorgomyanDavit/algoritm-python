19400f194e5bed2b6ded66342ffb04fc959550dd
David <torgomyandavid96@gmail.com>
2025-01-20 09:16:18 +0400

    master

--- sortingAlgoritms.py
+++ sortingAlgoritms.py
@@ -1,29 +1,55 @@
 
 
 arrayNum = [2, 1, 3, 5, 4]
+''' 
+    Bubble Sort repeatedly compares adjacent elements in an array and swaps them 
+    if they are in the wrong order. This process continues in multiple passes until 
+    no swaps are needed, meaning the array is sorted. During each pass, larger elements 
+    "bubble up" to their correct positions, starting from the beginning of the array.
+'''
 
-def bubleSort(input):
-    swipped = True
+# def bubleSort(input):
+#     swipped = True
+#     length = len(input)
+
+#     while(swipped):
+#         swipped = False
+
+#         for index in range(length - 1):
+#             currentElem = input[index]
+#             nextElem = input[index + 1]
+
+#             if currentElem > nextElem: 
+#                 input[index] = nextElem
+#                 input[index + 1] = currentElem
+#                 swipped = True
+
+#     return input
+
+        
+# print(f"currentElem {bubleSort(arrayNum)}")
+
+
+def SelectionSort(input):
     length = len(input)
+    for index in range(length - 1):
+        minIndex = index  # Assume the current index is the smallest
 
-    while(swipped):
-        swipped = False
+        # Find the smallest element in the unsorted portion
+        for ind in range(index + 1, length):
+            if input[ind] < input[minIndex]:
+                minIndex = ind
 
-        for index in range(length - 1):
-            currentElem = input[index]
-            nextElem = input[index + 1]
-
-            if currentElem > nextElem: 
-                input[index] = nextElem
-                input[index + 1] = currentElem
-                swipped = True
+        # Swap the smallest element with the first element of the unsorted portion
+        if minIndex != index:
+            elem = input[index]
+            input[index] = input[minIndex]
+            input[minIndex] = elem
 
     return input
 
-        
-print(f"currentElem {bubleSort(arrayNum)}")
-
-
+# Test the function
+print(f"Sorted list: {SelectionSort([3, 2, 5, 4, 1])}")
 
 
 
